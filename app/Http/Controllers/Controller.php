6054b49862fc89ecfcc4396850e73fe9647d0c87
Jahongir <alimovj727@gmail.com>
2025-04-05 13:24:40 +0500

    implement login functionality with token generation

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,5 +4,21 @@ namespace App\Http\Controllers;
 
 abstract class Controller
 {
-    //
+    protected function success($data = [], $message = 'Success', $status = 200): JsonResponse
+    {
+        return response()->json([
+            'success' => true,
+            'message' => __($message),
+            'data' => $data
+        ], $status);
+    }
+
+    protected function error($message = 'Error', $status = 400): JsonResponse
+    {
+        return response()->json([
+            'success' => false,
+            'message' => __($message),
+            'data' => null
+        ], $status);
+    }
 }
