0e2f6185640265c54d4b0a2e5462f4f633636377
Jahongir <alimovj727@gmail.com>
2025-04-05 13:39:21 +0500

    move auth validation to custom FormRequest classes

---
+++ lang/uz/massages.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'register_success' => 'Ro‘yxatdan o‘tish muvaffaqiyatli',
+    'login_success'    => 'Muvaffaqiyatli tizimga kirildi',
+    'logout_success'   => 'Tizimdan chiqildi',
+    'invalid_credentials' => 'Email yoki parol noto‘g‘ri',
+    'product_created'  => 'Mahsulot yaratildi',
+    'product_updated'  => 'Mahsulot yangilandi',
+    'product_deleted'  => 'Mahsulot o‘chirildi',
+    'products_list'    => 'Mahsulotlar ro‘yxati',
+];
